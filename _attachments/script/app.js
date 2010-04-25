e52c2a295bea84606c00d60ce547077a851045f6
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2010-04-25 21:10:37 +0800

    ajax hello

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -2,4 +2,26 @@ $.couch.app(function(app) {
   $("#hello").evently(app.ddoc.evently.hello, app);
 
   $("#account").evently(app.ddoc.vendor.couchapp.evently.account, app);
+
+  $("#ajax").evently({
+    _init: {
+      mustache: '<p>Loading CouchDB Server info.</p>',
+      after: function() {
+        var widget = $(this);
+        $.ajax({
+          url : '/',
+          complete: function(req) {
+            var resp = $.httpData(req, "json");
+            widget.trigger("version", [resp]);
+          }
+        })
+      }
+    },
+    version: {
+      mustache: "<p>Running CouchDB version {{version}}</p>",
+      data: function(e, resp) {
+        return resp;
+      }
+    }
+  });
 });
